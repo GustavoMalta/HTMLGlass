ff4aaf2d8335607554f16e4dd29754ac6722118a
GustavoMalta <gustavo.a.malta@gmail.com>
2019-06-23 00:30:54 -0300

    Commit

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,19 @@
+
+function mudaIcone(imagem){
+ 
+    document.getElementById("icone").src="_imagens/"+ imagem +".png";
+    document.getElementById("icone").hidden();
+}
+
+function rodape(){ 
+    document.getElementById("rodape").src = "../footer.html"
+}
+    /* "<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " You. All"
+	+ " rights reserved.</p>"
+	+ "<p id='credits'>Layout by You</p>"
+	+ "<p id='contact'><a href='mailto:you@you.com'>Contact Us</a> / "
+	+ "<a href='mailto:you@you.com'>Report a problem.</a></p>";
+    //$("#header").load("header.html");
+    //$("rodape").load("../footer.html"); 
+}*/
+
